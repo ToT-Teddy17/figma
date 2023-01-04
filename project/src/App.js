9a4302782c7bb8dfde98f9d2815203d07e34284c
Morning <morning@mstars-lab2-03s-iMac.local>
2023-01-04 12:42:54 +0800

    project hiih

---
+++ project/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import products from './Seed'
+import ProductFunc from './AppFunc';
+function App(){
+  
+
+return(
+  <div className='container'>
+    <div className="header">
+      <div>Need help? Call us: (+98) 0234 456 789</div>
+      <div className='header-right'>
+        <div>
+          <img src=''></img>
+          Our store
+        </div>
+        <div>
+          <img src=''></img>
+          track your order
+        </div>
+      </div>
+
+    </div>
+    
+  </div>
+);
+
+}
+
+
+
+
+export default App;
+

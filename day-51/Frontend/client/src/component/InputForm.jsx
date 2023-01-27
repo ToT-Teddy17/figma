735aa0eee03a54c6e7141b2bfd0236343ca2a664
Morning <morning@mstars-lab2-03s-iMac.local>
2023-01-27 11:18:34 +0800

    in day52

--- day-51/Frontend/client/src/component/InputForm.jsx
+++ day-51/Frontend/client/src/component/InputForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-export default function InputForm({ loaded, setLoaded }) {
+export default function InputForm({ loaded, setLoaded, setData }) {
+  const ATMOMENT_URL = "http://localhost:8080/data";
   async function postData(data) {
     console.log(data);
     console.log(typeof JSON.stringify(data));
@@ -12,6 +13,9 @@ export default function InputForm({ loaded, setLoaded }) {
       },
       body: JSON.stringify(data),
     };
+    const FETCHED_DATA = await fetch(ATMOMENT_URL, options);
+    const FETCHED_JSON = await FETCHED_DATA.json();
+    setData(FETCHED_JSON);
   }
 
   function handleSubmit(e) {

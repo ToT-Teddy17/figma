b8606988f397aeccb0be47758606a1a5e31748da
Morning <morning@mstars-lab2-03s-iMac.local>
2023-01-31 12:08:14 +0800

    day53

---
+++ admin-panel-project/fe/src/pages/Users.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import UsersTable from "../components/UsersTable";
+
+export default function Users() {
+  return (
+    <div>
+      <h1>Users Page</h1>
+      <UsersTable />
+    </div>
+  );
+}

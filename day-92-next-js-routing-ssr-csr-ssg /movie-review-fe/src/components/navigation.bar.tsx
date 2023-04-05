0f683717d9d73c643fcb8cf346b5968f5e7b914e
ToT-Teddy17 <monhvvlee14@gmail.com>
2023-04-05 12:32:53 +0800

    day92 img nemev

--- day-92-next-js-routing-ssr-csr-ssg /movie-review-fe/src/components/navigation.bar.tsx
+++ day-92-next-js-routing-ssr-csr-ssg /movie-review-fe/src/components/navigation.bar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function NavigationBar(): JSX.Element {
   return (
     <div>
-      <h1>Navigation Bar</h1>
+      <h1><img alt="Rotten Tomatoes" data-qa="header-logo" src="https://images.fandango.com/cms/assets/2d5a3340-be84-11ed-9d20-83ee649e98bd--rt25-logo-mainnav-161x50.svg"></img></h1>
     </div>
   );
 }
